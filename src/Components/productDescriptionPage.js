5beb0635a015fe7a68cb7b075fefa8489c377b78
jadapalligowd <jadapalli.gowd@bajajfinserv.in>
2023-06-30 17:43:30 +0530

    naya commit

---
+++ src/Components/productDescriptionPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import './PDP.css';
+
+const ProductDescriptionPage = ({ response }) => {
+  const { productId } = useParams();
+
+  const products = Object.values(response.Products);
+  const product = products.find((product) => product.productID === productId);
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <div className="product-description-page">
+      <div className="product-image">
+        <img src={product.image} alt={product.title} />
+      </div>
+      <div className="product-details">
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+        <p>Price: {product.price}</p>
+        <p>Product ID: {product.productID}</p>
+        <video src={product.video} controls />
+      </div>
+    </div>
+  );
+};
+
+export default ProductDescriptionPage;
---
+++ src/Router.js
@@ -0,0 +1,16 @@
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import HomePage from './App';
+// import ProductDescriptionPage from './Components/productDescriptionPage';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route exact path="/" component={HomePage} />
+//         <Route path="/product/:productId" component={ProductDescriptionPage} />
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// export default App;
